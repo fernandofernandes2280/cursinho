da10c2d87d95a9a9d26e0969a0a9dc0d9f8568f3
fernando <fernando@fernando-S400CA>
2022-05-23 15:12:17 -0300

    aulas ok ok

--- resources/view/admin/modules/aulas/req.php
+++
@@ -1,8 +0,0 @@
-<?php
-$pdo  = new PDO('mysql:host=localhost;dbname=cursinho', 'root', 'fl4m3ng0');
-$stmt = $pdo->prepare('select cidadeid, descricao from cidade where ufid=? order by descricao');
-$stmt->bindValue(1, ((int)$_POST['ufid']), PDO::PARAM_INT);
-$stmt->execute();
-$resultCidade = $stmt->fetchAll();
-$stmt->closeCursor();
-echo json_encode($resultCidade);
