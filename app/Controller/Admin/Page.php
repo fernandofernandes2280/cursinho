2eefe9a9b6a85987a2acb2dd671731929d30bbd5
fernando <fernando@fernando-S400CA>
2023-04-17 12:14:12 -0300

    alt menu presenca rapida

--- app/Controller/Admin/Page.php
+++ app/Controller/Admin/Page.php
@@ -47,7 +47,7 @@ class Page{
 	        
 	    ],
 	    'presencas' =>[
-	        'label' => 'Presenças',
+	        'label' => 'Presença Rápida',
 	        'link' => URL.'/admin/presencas',
 	        'material-icons' => 'checklist_rtl',
 	        'modal' => ''
